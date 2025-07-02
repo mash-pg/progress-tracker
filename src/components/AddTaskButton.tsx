29401eb183dbe765798eaf76f9a299fc219fbfa7
mash-pg <ymanshou@gmail.com>
2025-07-02 14:15:48 +0900

    fix button

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -8,7 +8,7 @@ export default function AddTaskButton({ onAddTask }: AddTaskButtonProps) {
   return (
     <button
       onClick={onAddTask}
-      className="add-task-button block w-48 sm:w-64 mx-auto py-3 sm:py-4 px-6 sm:px-8 bg-green-500 text-white rounded-full hover:bg-green-600 text-lg sm:text-xl font-bold shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+      className="add-task-button inline-block py-3 sm:py-4 px-6 sm:px-8 bg-green-500 text-white rounded-full hover:bg-green-600 text-lg sm:text-xl font-bold shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
     >
       新しいタスクを追加
     </button>
